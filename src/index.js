5f6da0fce3e36a6c55e95ab3558057b1adeea089
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-22 11:06:24 -0500

    Created Sognup Form

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/App';
 import Welcome from './components/Welcome';
+import Signup from './components/auth/Signup';
 
 ReactDOM.render(
   <BrowserRouter>
     <App>
       {/* whenever the App component is rendered, this Route will be passed to the App as a prop called children*/}
       <Route path='/' component={Welcome} />
+      <Route path='/signup' component={Signup} />
     </App>
   </BrowserRouter> , document.querySelector('#root')
 );
