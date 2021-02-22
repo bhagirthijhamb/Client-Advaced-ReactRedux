5cc73b1dae95e1a6d71b9c5a7df669b1cf291cd4
Bhagirthi Jhamb <bhagirthi.jhamb@gmail.com>
2021-02-22 10:08:46 -0500

    initial commit, App component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+// functional component as it won't need helper methods or component level state
+export default () => {
+  return (
+    <div>
+      Hi, i am the app
+    </div>
+  )
+}
